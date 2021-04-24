45a8f7ed1e9a933552b3a28d51ef77eb476050c0
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-04-24 17:39:31 +0800

    modify: some styles

--- src/components/mooze/form.jsx
+++ src/components/mooze/form.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Form = () => (
-  <div className={classNames('form-container')}>Start Your Mooze</div>
+  <div className={classNames('form-container')}>
+    <div className={classNames('form-title')}>Start Your Mooze</div>
+    <div>Project Name</div>
+  </div>
 );
 
 export default Form;
