8c17e51b3beea3a0d7cac854bfe89adf2ad9dd9e
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-04-24 15:31:32 +0800

    feat: nav layout

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -5,8 +5,10 @@ import NavLink from './link';
 const Nav = () => (
   <div className={classNames('nav-container')}>
     <NavLink path="/" name="DMooze" />
-    <NavLink path="/projects" name="PROJECTS" />
-    <NavLink path="/mooze" name="START YOUR MOOZE" />
+    <div className={classNames('nav-links')}>
+      <NavLink path="/projects" name="PROJECTS" />
+      <NavLink path="/mooze" name="START YOUR MOOZE" />
+    </div>
   </div>
 );
 
