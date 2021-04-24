5e61aa70ffafae8af38a8f5d94a088f24b7edd3f
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-04-24 17:48:50 +0800

    modify: some styles

--- src/components/mooze/form.jsx
+++ src/components/mooze/form.jsx
@@ -5,6 +5,15 @@ const Form = () => (
   <div className={classNames('form-container')}>
     <div className={classNames('form-title')}>Start Your Mooze</div>
     <div>Project Name</div>
+    <input type="text" />
+    <div>Owner Name</div>
+    <input type="text" />
+    <div>Email</div>
+    <input type="text" />
+    <div>Phone</div>
+    <input type="text" />
+    <div>Project Description</div>
+    <textarea />
   </div>
 );
 
