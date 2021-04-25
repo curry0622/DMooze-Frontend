183aafd39962348e5f128e600b00d5c88a884379
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-04-25 16:28:32 +0800

    feat: projects container prototype

---
+++ src/components/projects/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const ProjectsContainer = () => (
+  <div className={classNames('projects-container')}>ProjectsContainer</div>
+);
+
+export default ProjectsContainer;
