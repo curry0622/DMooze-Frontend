4bc6d63cd835079346fa5367df521aed35f88a17
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-04-25 23:32:49 +0800

    feat: project card with basic styles

--- src/components/projects/card.jsx
+++ src/components/projects/card.jsx
@@ -3,27 +3,27 @@ import classNames from 'classnames';
 
 const Card = () => (
   <div className={classNames('projects-card-container')}>
-    <div className={classNames('projects-card-header')}>header</div>
+    {/* <div className={classNames('projects-card-header')}>張凱傑</div> */}
     <img
       className={classNames('projects-card-img')}
       alt="project"
-      src={`${process.env.PUBLIC_URL}/projectsPage/sample.png`}
+      src={`${process.env.PUBLIC_URL}/projectsPage/sample.jpg`}
     />
     <div className={classNames('projects-card-content-container')}>
       <div className={classNames('projects-card-content-name')}>
-        Project name
+        區塊鏈募資平台
       </div>
       <div className={classNames('projects-card-content-progress')}>
         progress
       </div>
-      <div className={classNames('projects-card-content-info-container')}>
+      {/* <div className={classNames('projects-card-content-info-container')}>
         <div className={classNames('projects-card-content-info-money')}>
           $2400 | 12%
         </div>
         <div className={classNames('projects-card-content-info-day')}>
           還剩6天
         </div>
-      </div>
+      </div> */}
     </div>
   </div>
 );
