fcef54972e2600b188b467927460a53e735190f7
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-04-25 14:46:49 +0800

    feat: card hover change image

--- src/components/home/card.jsx
+++ src/components/home/card.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
-const Card = ({ title, img, description }) => (
-  <div
-    className={classNames('home-card-container')}
-    onMouseOver={() => {}}
-    onFocus={() => {}}
-  >
-    <div className={classNames('home-card-title')}>{title}</div>
-    <img
-      className={classNames('home-card-img')}
-      alt="person"
-      src={`${process.env.PUBLIC_URL}/homePage/${img}.png`}
-    />
-    <div className={classNames('home-card-description')}>{description}</div>
-  </div>
-);
+const Card = ({ title, img, description }) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div
+      className={classNames('home-card-container')}
+      onMouseEnter={() => {
+        setTimeout(() => setHover(true), 250);
+      }}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div className={classNames('home-card-title')}>{title}</div>
+      <img
+        className={classNames('home-card-img')}
+        alt="person"
+        src={`${process.env.PUBLIC_URL}/homePage/${img}${
+          hover ? '-hover' : ''
+        }.png`}
+      />
+      <div className={classNames('home-card-description')}>{description}</div>
+    </div>
+  );
+};
 
 Card.propTypes = {
   title: PropTypes.string.isRequired,
