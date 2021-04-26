f546145d5ad1a4b39a2e5387d7fd3fbdc48f852a
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-04-27 01:15:35 +0800

    feat: card progress bar

--- src/components/projects/card.jsx
+++ src/components/projects/card.jsx
@@ -13,12 +13,12 @@ const Card = () => (
       <div className={classNames('projects-card-content-name')}>
         區塊鏈募資平台
       </div>
-      {/* <div className={classNames('projects-card-content-progress')}>
-        progress
-      </div> */}
+      <div className={classNames('projects-card-content-progress')}>
+        <div className={classNames('projects-card-content-progress-value')} />
+      </div>
       <div className={classNames('projects-card-content-info-container')}>
         <div className={classNames('projects-card-content-info-money')}>
-          $2400 | 12%
+          $2400<span>12%</span>
         </div>
         <div className={classNames('projects-card-content-info-day')}>
           還剩6天
