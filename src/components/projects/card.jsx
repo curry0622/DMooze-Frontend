213ca08cea2b0401fd63956d00049f4af384a06a
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-04-27 02:02:22 +0800

    feat: sample project image

--- src/components/projects/card.jsx
+++ src/components/projects/card.jsx
@@ -1,31 +1,45 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-const Card = () => (
-  <div className={classNames('projects-card-container')}>
-    {/* <div className={classNames('projects-card-header')}>張凱傑</div> */}
-    <img
-      className={classNames('projects-card-img')}
-      alt="project"
-      src={`${process.env.PUBLIC_URL}/projectsPage/sample.jpg`}
-    />
-    <div className={classNames('projects-card-content-container')}>
-      <div className={classNames('projects-card-content-name')}>
-        區塊鏈募資平台
-      </div>
-      <div className={classNames('projects-card-content-progress')}>
-        <div className={classNames('projects-card-content-progress-value')} />
-      </div>
-      <div className={classNames('projects-card-content-info-container')}>
-        <div className={classNames('projects-card-content-info-money')}>
-          $2400<span>12%</span>
+const Card = ({ name, img, price, target, day }) => {
+  const progress = ((price / target) * 100).toFixed(0);
+
+  return (
+    <div className={classNames('projects-card-container')}>
+      <img
+        className={classNames('projects-card-img')}
+        alt="project"
+        src={`${process.env.PUBLIC_URL}/projectsPage/${img}.png`}
+      />
+      <div className={classNames('projects-card-content-container')}>
+        <div className={classNames('projects-card-content-name')}>{name}</div>
+        <div className={classNames('projects-card-content-progress')}>
+          <div
+            className={classNames('projects-card-content-progress-value')}
+            style={{ left: `${progress}%` }}
+          />
         </div>
-        <div className={classNames('projects-card-content-info-day')}>
-          還剩6天
+        <div className={classNames('projects-card-content-info-container')}>
+          <div className={classNames('projects-card-content-info-money')}>
+            ${price}
+            <span>{progress}%</span>
+          </div>
+          <div className={classNames('projects-card-content-info-day')}>
+            還剩{day}天
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  target: PropTypes.number.isRequired,
+  day: PropTypes.number.isRequired,
+};
 
 export default Card;
--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -7,9 +7,27 @@ const ProjectsContainer = () => (
   <div className={classNames('projects-container')}>
     <Banner />
     <div className={classNames('projects-cards-container')}>
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        name="R3 Corda 區塊鏈培訓"
+        img="sample1"
+        price={65300}
+        target={72000}
+        day={30}
+      />
+      <Card
+        name="成大資訊營"
+        img="sample2"
+        price={25020}
+        target={63400}
+        day={21}
+      />
+      <Card
+        name="Homework Tutor"
+        img="sample3"
+        price={14310}
+        target={125000}
+        day={8}
+      />
     </div>
   </div>
 );
