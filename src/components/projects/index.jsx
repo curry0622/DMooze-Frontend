52477ac4864382c5913c064a2f8be7f3fd31938c
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-04-27 11:34:25 +0800

    feat: click project card to route to its info page

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -8,6 +8,7 @@ const ProjectsContainer = () => (
     <Banner />
     <div className={classNames('projects-cards-container')}>
       <Card
+        addr="0x61429Ed5bd82b8074d1A98023aEe82a4cF6D8322"
         name="R3 Corda 區塊鏈培訓"
         img="sample1"
         price={65300}
@@ -15,6 +16,7 @@ const ProjectsContainer = () => (
         day={30}
       />
       <Card
+        addr="0x869bE608B7a4C468b9d40C397818ED07e7Ff75de"
         name="成大資訊營-終局資戰"
         img="sample2"
         price={25020}
@@ -22,6 +24,7 @@ const ProjectsContainer = () => (
         day={21}
       />
       <Card
+        addr="0xFf1Bd48d673A6E15eDd4AA24603386d60Bc2FDFf"
         name="Homework Tutor"
         img="sample3"
         price={14310}
