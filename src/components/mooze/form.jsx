2b9e2988f475330b9cc54daf95783ea1928bbf1b
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-05-12 22:29:41 +0800

    feat: form with formik, material ui

--- src/components/mooze/form.jsx
+++ src/components/mooze/form.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable no-alert */
+/* eslint-disable react/button-has-type */
 import React from 'react';
-import { Formik, Field, Form } from 'formik';
+import { useFormik, Formik, Field, Form } from 'formik';
+import { createMuiTheme, ThemeProvider, TextField } from '@material-ui/core';
 import * as Yup from 'yup';
 import classNames from 'classnames';
+import { create } from 'yup/lib/array';
 
 const MoozeForm = () => {
   const initialValues = {
@@ -13,18 +17,103 @@ const MoozeForm = () => {
     description: '',
   };
 
+  const onSubmit = (values, actions) => {
+    alert(JSON.stringify(values));
+    actions.resetForm({
+      values: initialValues,
+    });
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  const theme = createMuiTheme({
+    typography: {
+      fontFamily: 'Noto Serif TC, serif',
+    },
+  });
+
   return (
-    <Formik initialValues={initialValues}>
-      <Form className={classNames('form-container')}>
-        <div className={classNames('form-title')}>Start Your Mooze</div>
-        <Field name="name" type="text" placeholder="name" />
-        <Field name="owner" type="text" placeholder="owner" />
-        <Field name="target" type="number" placeholder="target" />
-        <Field name="email" type="email" placeholder="email" />
-        <Field name="phone" type="tel" placeholder="phone" />
-        <Field name="description" as="textarea" placeholder="description" />
-      </Form>
-    </Formik>
+    <form
+      className={classNames('form-container')}
+      onSubmit={formik.handleSubmit}
+      onReset={formik.handleReset}
+    >
+      <div className={classNames('form-title')}>開始提案</div>
+      <ThemeProvider theme={theme}>
+        <TextField
+          name="name"
+          label="專案名稱"
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          variant="outlined"
+          type="text"
+          size="small"
+          fullWidth
+        />
+        <TextField
+          name="owner"
+          label="專案擁有者"
+          value={formik.values.owner}
+          onChange={formik.handleChange}
+          variant="outlined"
+          type="text"
+          size="small"
+          fullWidth
+        />
+        <TextField
+          name="target"
+          label="目標金額"
+          value={formik.values.target}
+          onChange={formik.handleChange}
+          variant="outlined"
+          type="number"
+          size="small"
+          fullWidth
+        />
+        <TextField
+          name="email"
+          label="電子信箱"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          variant="outlined"
+          type="text"
+          size="small"
+          fullWidth
+        />
+        <TextField
+          name="phone"
+          label="連絡電話"
+          value={formik.values.phone}
+          onChange={formik.handleChange}
+          variant="outlined"
+          type="tel"
+          size="small"
+          fullWidth
+        />
+        <TextField
+          name="description"
+          label="專案描述"
+          value={formik.values.description}
+          onChange={formik.handleChange}
+          variant="outlined"
+          type="text"
+          size="small"
+          fullWidth
+          multiline
+        />
+      </ThemeProvider>
+      <div>
+        <button type="submit" className={classNames('form-btn')}>
+          提交
+        </button>
+        <button type="reset" className={classNames('form-btn')}>
+          重設
+        </button>
+      </div>
+    </form>
   );
 };
 
